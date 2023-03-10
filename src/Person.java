60fab0bd3c308c50076cf76839d35ea37b1a8d68
wsw2025 <shuwenw2025@student.cis.edu.hk>
2023-03-10 08:47:45 +0800

    ^^

--- src/Person.java
+++ src/Person.java
@@ -4,4 +4,32 @@ public class Person {
     private String lastName;
     private int age;
 
+    public Person(String firstName, String lastName, int age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+    public String getLastName(){
+        return this.lastName;
+    }
+    public int getAge(){
+        return this.age;
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public String toString(){
+        return this.lastName + " " + this.firstName + ", " + this.age;
+    }
+
 }
--- src/PersonFactory.java
+++ src/PersonFactory.java
@@ -9,14 +9,37 @@ public class PersonFactory {
     }
 
     public void addPerson(Person person) {
+        if(allPersons.isEmpty()){ //if no one in allPersons arraylist yet
+            allPersons.add(person);
+            return;
+        }
+        for (int i = 0; i < allPersons.size(); i++){
+            if(allPersons.get(i).getLastName().compareTo(person.getLastName())==1){
+                allPersons.add(i, person);
+                System.out.println(i);
+                return;
+            }
+        }
 
+        allPersons.add(person); //if no last name in allPersons is lexicographically larger than person, add person to end
     }
 
     public ArrayList<Person> under18() {
-
+        ArrayList<Person> minor = new ArrayList<>();
+        for (int i = 0; i < allPersons.size(); i++){
+            if(allPersons.get(i).getAge()<18){
+                minor.add(allPersons.get(i));
+            }
+        }
+        return minor;
     }
 
     public boolean checkLastName(String lastName) {
-
+        for (int i = 0; i < allPersons.size(); i++){
+            if(allPersons.get(i).getLastName().equals(lastName)){
+                return true;
+            }
+        }
+        return false;
     }
 }
